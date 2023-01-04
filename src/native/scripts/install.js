adf98523156028c3ba7bf679319465f249ae0e49
HaseenaSainul <h.sainul@metrological.com>
2023-01-04 06:58:32 -0800

    CoreSDK: merged build:csdk header file and header install support added

--- src/native/scripts/install.js
+++ src/native/scripts/install.js
@@ -1,43 +1,32 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const fs = require('fs');
+const path = require('path');
 
-var dstPath = process.env.NODE_INSTALL_PATH;
-var name = process.env.NATIVE_LIB_NAME;
-var versionStr = process.env.NATIVE_LIB_VERSION;
-var version = versionStr.split(".");
+var dest = process.env.NODE_INSTALL_PATH;
+var src = 'src/native/build/' + process.env.TARGET_NAME;
 
-var libName = 'lib' + name + '.so.' + version[0] + '.' + version[1] + '.' + version[2];
-var linkName = 'lib' + name + '.so.' + version[0];
-
-var srcPath = 'src/native/sdk/build/'
-var buildType = 'Release/';
-createDir(dstPath + '/usr/lib/pkgconfig');
-createDir(dstPath + '/etc');
-
-copyFile(srcPath + 'Source/' + 'FireboltSDK.pc', dstPath + '/usr/lib/pkgconfig/FireboltSDK.pc');
-copyFile(srcPath + 'Source/' + 'config/FireboltSDK.json', dstPath + '/etc/FireboltSDK.json');
-copyFile(srcPath + buildType + libName, dstPath + '/usr/lib/' + libName);
-
-createFileLink(libName, dstPath + '/usr/lib/' + linkName);
-createFileLink(linkName, dstPath + '/usr/lib/libFireboltSDK.so');
-
-function copyFile(src, dst) {
-    fs.copyFileSync(src, dst);
-    console.log(src + ' is copied to ' + dst);
-}
-
-function createFileLink(src, dst) {
-    if (fs.existsSync(dst)) {
-       fs.unlinkSync(dst);
+installFiles(src, dest);
+function installFiles(src, dest) {
+    if (!fs.existsSync(dest)) {
+        fs.mkdirSync(dest);
     }
-    fs.symlinkSync(src, dst);
-    console.log(src + ' symlink is created to ' + dst);
-}
 
-function createDir(dir) {
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-        console.log('Directory ' + dir + ' is created.')
-    }
+    var entries = fs.readdirSync(src);
+    entries.forEach((entry) => {
+        const srcPath = path.join(src, entry);
+        const destPath = path.join(dest, entry);
+        const stat = fs.lstatSync(srcPath);
+
+        if (stat.isFile()) {
+            fs.copyFileSync(srcPath, destPath);
+        } else if (stat.isDirectory()) {
+            installFiles(srcPath, destPath);
+        } else if (stat.isSymbolicLink()) {
+            if (fs.existsSync(destPath)) {
+                fs.unlinkSync(destPath);
+	    }
+            fs.symlinkSync(fs.readlinkSync(srcPath), destPath);
+        }
+    });
 }
