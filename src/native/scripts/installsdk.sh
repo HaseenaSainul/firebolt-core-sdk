bfe6bbb209946aa416cac2fd10c84a4bf1d27a3a
HaseenaSainul <h.sainul@metrological.com>
2023-05-05 02:59:14 -0700

    included generated files also part of release and rename release to firebolt-core-native-sdk

--- src/native/scripts/installsdk.sh
+++ src/native/scripts/installsdk.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-ReleasePath=./build/sdk/native-c/release/
+ReleasePath=./build/sdk/native-c/firebolt-core-native-sdk/
 rm -rf ${ReleasePath}
 mkdir -p ${ReleasePath}/core
 cp -ar ./src/native/src ${ReleasePath}/core
 cp -ar ./src/native/test ${ReleasePath}/core
 cp -ar ./src/native/CMakeLists.txt ${ReleasePath}/core
 cp -a ./src/native/build-core.sh ${ReleasePath}/core
+cp -a ${ReleasePath}/../generated ${ReleasePath}/core
 
 mkdir -p ${ReleasePath}/openrpc
-cp -ar ../firebolt-openrpc/build/sdk/native-c/release/* ${ReleasePath}/openrpc
+cp -ar ../firebolt-openrpc/build/sdk/native-c/firebolt-openrpc-native-sdk/* ${ReleasePath}/openrpc
 
 cd ${ReleasePath}../
-tar -cvzf release.tgz release
+tar -cvzf firebolt-core-native-sdk.tgz firebolt-core-native-sdk/*
 cd -
